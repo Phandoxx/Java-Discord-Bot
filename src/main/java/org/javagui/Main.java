e2182c04af201fcfa0c62ef9fccc8373595d2d66
Phandoxx <143786592+Phandoxx@users.noreply.github.com>
2025-10-16 20:07:55 +1300

    panels

--- src/main/java/org/javagui/Main.java
+++ src/main/java/org/javagui/Main.java
@@ -8,31 +8,44 @@ public class Main {
     static void main() {
         setup();
 
+        MainFrame MainFrame = new MainFrame();
+        MainFrame.setLayout(new BorderLayout());
+
+        JPanel settingsPanel = new JPanel();
+        JPanel mainPanel = new JPanel();
+
+        settingsPanel.setBackground(Color.DARK_GRAY);
+        mainPanel.setBackground(Color.GRAY);
+
+        settingsPanel.setPreferredSize(new Dimension(100,50));
+
         ImageIcon image = new ImageIcon("files/logo/amd.png");
         Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
 
-        JLabel label = new JLabel();
-        label.setText("AMD logo (its just a random image i had)");
-        label.setIcon(image);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.TOP);
-        label.setFont(new Font("Times New Roman", Font.BOLD, 16));
-        label.setForeground(Color.GREEN);
-        label.setIconTextGap(0);
-        label.setBackground(Color.BLACK);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        //label.setBounds(0, 0, 500, 500);
 
-        MyFrame myFrame = new MyFrame();
+        MainFrame.add(settingsPanel, BorderLayout.NORTH);
+        MainFrame.add(mainPanel, BorderLayout.CENTER);
+        MainFrame.setSize(750,500);
+        MainFrame.setTitle("Java GUI Experiment");
+        MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MainFrame.setResizable(false);
 
-        //myFrame.setLayout(null);
-        myFrame.setVisible(true);
-        myFrame.add(label);
+        JLabel mainLabel = new JLabel("AMD logo (it's just a random image I had)", image, JLabel.CENTER);
+        mainLabel.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        mainLabel.setForeground(Color.GREEN);
+        mainLabel.setHorizontalTextPosition(JLabel.CENTER);
+        mainLabel.setVerticalTextPosition(JLabel.BOTTOM);
+        mainLabel.setIconTextGap(0);
+        mainLabel.setHorizontalAlignment(JLabel.CENTER);
+        mainLabel.setVerticalAlignment(JLabel.CENTER);
 
-        myFrame.pack(); //use last
+        // Add the label to the panel
+        mainPanel.add(mainLabel, BorderLayout.CENTER);
+
+
+
+        MainFrame.setVisible(true);
+        //myFrame.pack(); //use last
 
     }
     public static void setup() {
---
+++ src/main/java/org/javagui/MainFrame.java
@@ -0,0 +1,11 @@
+package org.javagui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    MainFrame(){
+        //
+
+    }
+}
